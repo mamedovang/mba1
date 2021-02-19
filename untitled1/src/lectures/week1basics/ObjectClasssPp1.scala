60e87ac56f665b6176377d0fd2fb929b507fd259
hdmaster <mamedovang@mgpu.ru>
2021-02-19 20:34:37 +0300

    Practicheskaya 1

---
+++ untitled1/src/lectures/week1basics/ObjectClasssPp1.scala
@@ -0,0 +1,7 @@
+package lectures.week1basics
+
+object ObjectClasssPp1 {
+  val upperCaseVal = "example".toUpperCase
+  val  res= upperCaseVal.take(2)
+ println(2)
+}
---
+++ untitled1/src/lectures/week1basics/package1.scala
@@ -0,0 +1,6 @@
+package lectures.week1basics
+
+object package1 {
+var aString: String = "Hello, Word"
+  println (aString.toUpperCase)
+}
